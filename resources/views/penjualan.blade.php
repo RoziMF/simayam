1769e19b67705ffc235d62577eae18c6a9beca53
Andreas Pandega <pandegaf@gmail.com>
2019-10-07 16:02:04 +0700

    First commit

--- resources/views/penjualan.blade.php
+++ resources/views/penjualan.blade.php
@@ -2,45 +2,7 @@
 
 @section('content')
 
-@if(Auth::user()->type == '1')
-<section class="content-header">
-  <h1>
-    Pembelian
-  </h1>
-  <ol class="breadcrumb">
-    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    <li class="active">Pembelian</li>
-  </ol>
-</section>
-<section class="content">
 
-  <!-- Default box -->
-  <div class="box">
-    <div class="box-header with-border">
-      <h3 class="box-title">Riwayat Pembelian</h3>
-
-      <div class="box-tools pull-right">
-        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                title="Collapse">
-          <i class="fa fa-minus"></i></button>
-        <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-          <i class="fa fa-times"></i></button>
-      </div>
-    </div>
-    <div class="box-body">
-      Lihat Riwayat Pembelian
-    </div>
-    <!-- /.box-body -->
-    <!-- <div class="box-footer">
-      Footer
-    </div> -->
-    <!-- /.box-footer-->
-  </div>
-  <!-- /.box -->
-
-</section>
-
-@else
 <section class="content-header">
   <h1>
     Penjualan
@@ -107,6 +69,6 @@
 
 </section>
 
-@endif
+
 
 @endsection
